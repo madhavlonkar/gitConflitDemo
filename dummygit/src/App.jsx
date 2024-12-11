cd71471e8693d520169b171c32a9df505e3fba59
Vaishnavi Nanna <nannavaishnavi9049@gmail.com>
2024-12-11 15:44:49 +0530

    HMR 10

--- dummygit/src/App.jsx
+++ dummygit/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR5
+          Edit <code>src/App.jsx</code> and save to test HMR10
         </p>
       </div>
       <p className="read-the-docs">
